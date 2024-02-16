f0c57ab8006d0b68c875117bd9ac1dd003d0c182
Pamela Alba <ada_hack537@womenintech.mx>
2024-02-16 15:10:06 -0600

    otros cambios

--- src/main.jsx
+++ src/main.jsx
@@ -16,9 +16,9 @@ const feed = document.querySelector('.contenedor-galery');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 
-const token = 'IGQWRNVlAxZATYwUmtBRGlJcWFxZAEhQejY5SHQwQ2xPTnR2OFQzWjRSUnZAmbHhLSFBvdmhvOVplczV0UUJOZA0pJXzVRdUt0UG1Halozckw1bE91UWNmeHo0TXNHWU4xbTBpTzZAZAZAHR2YUNzOTd3Mm1ZAaVh2akQxM3cZD' 
+const token = 'IGQWRQNkxNQW1lU1RhdW9kVUZAnN1ZAfQlF2QV9PQ1dNNnpUb3VEeV9uSVZAPNDdqbDRad2FsT3RIWTk2Tzhwb2UzN3lSVktpZAkVuNmY5M2xLeHNZATkhzUmlnU082SzdJNGM3b3Q5dktacTBnN2xHdUJYbm5tRW1yRFEZD' 
 
-const url = 'https://graph.facebook.com/v3.2/17841405309211844?fields=business_discovery.username(pam.alba08){followers_count,media_count}&access_token={IGQWRNVlAxZATYwUmtBRGlJcWFxZAEhQejY5SHQwQ2xPTnR2OFQzWjRSUnZAmbHhLSFBvdmhvOVplczV0UUJOZA0pJXzVRdUt0UG1Halozckw1bE91UWNmeHo0TXNHWU4xbTBpTzZAZAZAHR2YUNzOTd3Mm1ZAaVh2akQxM3cZD}'
+const url = 'https://graph.facebook.com/v3.2/17841405309211844?fields=business_discovery.username(pam.alba08){followers_count,media_count}&access_token={IGQWRQNkxNQW1lU1RhdW9kVUZAnN1ZAfQlF2QV9PQ1dNNnpUb3VEeV9uSVZAPNDdqbDRad2FsT3RIWTk2Tzhwb2UzN3lSVktpZAkVuNmY5M2xLeHNZATkhzUmlnU082SzdJNGM3b3Q5dktacTBnN2xHdUJYbm5tRW1yRFEZD}';
 
 fetch(url)
 .then(rest => rest.json())
